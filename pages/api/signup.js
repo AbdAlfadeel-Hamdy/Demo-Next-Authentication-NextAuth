6aaa989a752a9278a8b2051cb7eb62721f511995
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-03-03 11:45:06 +0200

    adding user sign-up API route

---
+++ pages/api/signup.js
@@ -0,0 +1,27 @@
+const { hashPassword } = require("@/lib/auth");
+const { connectToDatabase } = require("@/lib/db");
+
+const handler = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !email.includes("@") || !password || password.length < 7) {
+    res.status(422).json({
+      message:
+        "Invalid input - password should also be at least 7 charachters long",
+    });
+  }
+
+  const client = await connectToDatabase();
+
+  const db = client.db();
+
+  const hashedPassword = await hashPassword(password);
+
+  const result = await db.collection("users").insertOne({
+    email,
+    password: hashedPassword,
+  });
+  res.status(201).json({ message: "Created user!" });
+};
+
+export default handler;
