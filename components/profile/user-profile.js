1ab0e17ff28bd52734528b558dca757a589ad943
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-03-03 16:53:20 +0200

    client side page guards

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,9 +1,18 @@
-import ProfileForm from './profile-form';
-import classes from './user-profile.module.css';
+import { getSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import ProfileForm from "./profile-form";
+import classes from "./user-profile.module.css";
 
 function UserProfile() {
-  // Redirect away if NOT auth
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session) window.location.href = "/auth";
+      else setIsLoading(false);
+    });
+  });
+  if (isLoading) return <div className={classes.profile}>Loading...</div>;
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
